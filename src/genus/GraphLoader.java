cc21cdbfc7477fc52890ceeef53b01fdaa318805
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-10-09 15:47:49 +0200

    Added robrecht style file format reading.

--- src/genus/GraphLoader.java
+++ src/genus/GraphLoader.java
@@ -21,14 +21,25 @@ public class GraphLoader
     public void load(String fileName)
     {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader =
+                    new BufferedReader(new FileReader(fileName));
             try {
                 String line = reader.readLine();
                 while(line != null) {
-                    int space = line.indexOf(' ', 0);
-                    int vertex0 = Integer.parseInt(line.substring(0, space));
-                    int vertex1 = Integer.parseInt(line.substring(space + 1));
-                    graph.addEdge(vertex0, vertex1);
+                    int split = line.indexOf("<->", 0);
+
+                    String[] from = line.substring(0, split).split("[, ]+");
+                    String[] to = line.substring(split + 3).split("[, ]+");
+
+                    for(String fromVertex: from) {
+                        for(String toVertex: to) {
+                            if(!fromVertex.equals(toVertex)) {
+                                graph.addEdge(Integer.parseInt(fromVertex),
+                                        Integer.parseInt(toVertex));
+                            }
+                        }
+                    }
+
                     line = reader.readLine();
                 }
             } finally {
