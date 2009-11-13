7fd87be6c7398e97c50d6aef6a1a8a9a0f97ef8c
Jasper Van der Jeugt <jaspervdj@gmail.com>
2009-11-13 16:34:45 +0100

    Wrote about nodes with 1 neighbout in report.

---
+++ src/genus/CountBranchesFindGenus.java
@@ -0,0 +1,28 @@
+package genus;
+import java.util.Set;
+
+/** An implementation of FindGenus used to examine the search tree.
+ */
+public class CountBranchesFindGenus extends DefaultFindGenus
+{
+    int branches;
+
+    @Override
+    public void onRecursionStart(DefaultGraph graph)
+    {
+        super.onRecursionStart(graph);
+        branches = 0;
+    }
+
+    @Override
+    public void onRecursionEnd(int faces)
+    {
+        branches++;
+    }
+
+    @Override
+    public void afterRecursion()
+    {
+        System.out.println("Branches: " + branches);
+    }
+}
